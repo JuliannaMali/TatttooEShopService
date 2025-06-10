5fa9c865199a8b10c26df054430c74eeb8348b32
JuliannaMali <mali.julianna6@gmail.com>
2025-06-10 17:03:47 +0200

    Dodaj pliki projektów.

---
+++ TattooEShopDomain/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace TattooEShopDomain.Models;
+public class Product
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Ean { get; set; } = string.Empty;
+
+    public decimal Price { get; set; }
+
+    public int Stock { get; set; } = 0;
+
+    public string Sku { get; set; } = string.Empty;
+
+    public Category Category { get; set; } = default!;
+
+}
---
+++ TattooEShopDomain/Repositories/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using TattooEShopDomain.Models;
+using Microsoft.EntityFrameworkCore;
+namespace TattooEShopDomain.Repositories;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    public DbSet<Product> Products { get; set; }
+}
